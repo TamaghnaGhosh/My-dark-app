fda06cab785aab962cd58f3f2a7f7ba8046c2569
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2026-03-10 23:56:04 +0530

    Prepare codebase for is-a.dev custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
-  basePath: isProd ? "/My-dark-app" : "",
   images: {
     unoptimized: true,
   },

bb2b7c2ce480352cfb850fcdee9819c7af39b7ab
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2026-03-11 00:22:56 +0530

    nextConfig file updated

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
+  basePath: isProd ? "/My-dark-app" : "",
   images: {
     unoptimized: true,
   },
